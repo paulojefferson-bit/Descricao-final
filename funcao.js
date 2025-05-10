017b6ca5e770a7d304ea9f9a94f3413c64809f66
jvictor29 <joaobdops@gmail.com>
2025-05-10 01:09:39 -0300

    nova atualizacao home

---
+++ funcao.js
@@ -0,0 +1,29 @@
+//joao victor
+  document.addEventListener("DOMContentLoaded", function () {
+    let banner = document.getElementById("cookieBanner"); 
+    let acceptBtn = document.getElementById("acceptCookies"); 
+
+    if (!localStorage.getItem("cookiesAccepted")) {
+      banner.style.display = "flex"; 
+    }    
+    acceptBtn.addEventListener("click", function () {
+      localStorage.setItem("cookiesAccepted", "true"); 
+      banner.style.display = "none"; 
+    });
+  });
+
+
+
+
+
+
+
+
+//Thiago
+
+
+
+
+
+
+//Jefferson
