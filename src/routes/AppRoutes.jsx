759734c0c1773dde6f5765b6148e9acfa839a8b3
edgthiago <thiagoeudev@gmail.com>
2025-05-25 21:40:01 -0300

    adicionei a imagem dos produtos e corrigi a roda de categorias

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -14,13 +14,13 @@ const AppRoutes = () => {
       <Route path="/produtos" element={<PaginaProdutos />} />
       <Route path="/produtos/:id" element={<PaginaDetalhesProduto />} />
       <Route path="/carrinho" element={<PaginaCarrinho />} />
-      <Route path="/categorias" element={<PaginaProdutos />} />
+      <Route path="/categorias" element={<h1 className="text-center mt-5">Página em desenvolvimento</h1>} /> 
       <Route path="/meus-pedidos" element={<h1 className="text-center mt-5">Meus Pedidos</h1>} />
       <Route path="/sobre" element={<h1 className="text-center mt-5">Sobre</h1>} />
       <Route path="/contato" element={<h1 className="text-center mt-5">Contato</h1>} />
       <Route path="/termos" element={<h1 className="text-center mt-5">Termos e Condições</h1>} />
       <Route path="/devolucoes" element={<h1 className="text-center mt-5">Trocas e Devoluções</h1>} />
-      <Route path="*" element={<h1 className="text-center mt-5">Página não encontrada</h1>} /> 
+      <Route path="*" element={<h1 className="text-center mt-5">Página em desenvolvimento</h1>} /> 
     </Routes>
   );
 };
