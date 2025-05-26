d6fac3714e7873b6b84d2ae44843f457452459b5
jvictor29 <joaobdops@gmail.com>
2025-05-25 21:01:06 -0300

    correcaocadastro

--- src/components/Cadastro/Cadastro.jsx
+++ src/components/Cadastro/Cadastro.jsx
@@ -57,8 +57,8 @@ export default function Login() {
             <div className="text-center mt-4">
               <small className="text-muted">Ou faça login com</small>
               <div className="d-flex justify-content-center gap-3 mt-2">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/4/4e/Microsoft_logo.svg" alt="Microsoft" height="20" />
-                <img src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" alt="Google" height="20" />
+                <img src="img/Microsoft_logo.svg.png" alt="Microsoft" height="20" />
+                <img src="img/Google__G__logo.svg.png" alt="Google" height="20" />
                 <img src="https://upload.wikimedia.org/wikipedia/commons/0/05/Facebook_Logo_%282019%29.png" alt="Facebook" height="20" />
               </div>
             </div>
@@ -67,10 +67,10 @@ export default function Login() {
           {/* Imagem */}
           <div className="col-md-6 d-none d-md-flex align-items-center justify-content-center bg-light">
             <img
-              src="https://static.nike.com/a/images/t_prod_ss/w_960,c_limit,f_auto/cdbba6e5-6e6f-4590-aac5-3b4d8e9c92ff/nike-air-max-2021-shoes.png"
+              src="img/cadastro.png"
               alt="Tênis"
               className="img-fluid p-4"
-              style={{ maxHeight: '350px' }}
+              style={{ maxHeight: '700px' }}
             />
           </div>
         </div>
